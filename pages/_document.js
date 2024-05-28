65e93aa3577bd846ed1e1333eb1728c1fce66933
herpichrmit <s3935413@student.rmit.edu.au>
2024-05-28 23:38:34 +1000

    updated title and favicon

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.png" />
+        <title>Ethan Herpich</title>
+      </Head>
       <body className="no-scrollbar">
         <Main />
         <NextScript />
